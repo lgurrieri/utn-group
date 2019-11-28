bb1b1e397fbb34f875437867e699c9b9bd9d4679
sebaf96 <sefernandez96@gmail.com>
2019-11-28 19:46:52 -0300

    agregando system out

--- Proyecto/src/main/App.java
+++ Proyecto/src/main/App.java
@@ -4,8 +4,10 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Group 6");
+
 		System.out.println("Hello my name is Facundo");
-		
+
+		System.out.println("Ceeeeeeeeeeeeeeeeeeeeeeeeeeeeb!");
 	}
 
 }
